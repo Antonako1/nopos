6ea0742ad1cf01e75b11ede8ee6ae63327e7d894
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2026-09-24 01:16:09 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
+# set -euo pipefail
 
+
+# sudo ip addr add 192.168.10.1/24 dev eth1 
 ./build.sh
-sudo IFACE=eth1 SERVER_IP=192.168.10.1 ./pxe/serve.sh
+# sudo ip link set eth1 up
+# ip addr show eth1
+
+#sudo ip addr flush dev eth2
+#sudo ip addr add 192.168.10.1/24 dev eth2
+#sudo ip link set eth2 up
+
+sudo IFACE=eth0 SERVER_IP=192.168.10.1 ./pxe/serve.sh
